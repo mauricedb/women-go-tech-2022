5a151e55448607adc3d8692664a9a31f10b8c82d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-05-26 12:15:56 +0200

    Image Optimization

--- src/movie-list.tsx
+++ src/movie-list.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import { MovieInterface } from './types';
 
@@ -13,10 +14,12 @@ const MovieList: React.FC<Props> = ({ movies }) => {
         <div key={movie.id} className="card mb-3" style={{ maxWidth: 620 }}>
           <div className="row g-0">
             <div className="col-md-4">
-              <img
+              <Image
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                 className="img-fluid rounded-start"
                 alt={movie.title}
+                height="309"
+                width="206"
               />
             </div>
             <div className="col-md-8">
